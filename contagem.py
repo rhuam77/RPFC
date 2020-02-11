23dbbbb10c07a6d90b6b404bff30b1c4ccc45fb3
rhuam77 <60892982+rhuam77@users.noreply.github.com>
2020-02-10 22:17:05 -0300

    Teste em formato python
    Ola pessoal... Estou aprendendo a usar o GitHub e esses comandos sao apenas pra teste ate eu conseguir uma experiencia melhor.  :)

---
+++ contagem.py
@@ -0,0 +1,6 @@
+#ola pessoal... Estou aprendendo a usar o GitHub e esses comandos sao apenas pra teste ate eu conseguir uma experiencia melhor.  :)
+
+from time import sleep
+for cont in range(0, 10):
+    print(cont)
+    sleep (0.5)
